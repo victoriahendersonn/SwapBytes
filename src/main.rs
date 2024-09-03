6dacac37c509a47a9a5fcebdc3f8af8e799fada2
vhe20 <vhe20@uclive.ac.nz>
2024-09-03 23:04:56 +1200

    working dm, global chat.

--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,15 @@ use tokio::{
     select, spawn,
 };
 
-use std::io::{Write};
-use termios::{tcsetattr, Termios, ECHO, ICANON, TCSANOW};
+use color_eyre::Result;
+use ratatui::{
+    crossterm::event::{self, Event, KeyCode, KeyEventKind},
+    layout::{Constraint, Layout, Position},
+    style::{Color, Modifier, Style, Stylize},
+    text::{Line, Span, Text},
+    widgets::{Block, List, ListItem, Paragraph},
+    DefaultTerminal, Frame,
+};
 
 mod network;
 
@@ -154,27 +161,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
         })?
         .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(60)))
         .build();
-    // this builds up a swarm. using gossip sub -> a chat room where people can subscribes to topics and if a msg
-    // gets sent then all subscribers will receive the msg :) (propagates)
 
-    // kademlia
-    swarm
-        .behaviour_mut()
-        .kademlia
-        .set_mode(Some(kad::Mode::Server));
-
-    // will listen on local host over two different protocols (UDP and TCP)
-    // will randomly select a port for TCP
-    //swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
-    // QUIC is built on top of UDP -> we have to tell it that it has to be through QUIC
+    swarm.behaviour_mut().kademlia.set_mode(Some(kad::Mode::Server));
     swarm.listen_on("/ip4/0.0.0.0/udp/0/quic-v1".parse()?)?;
-    // ^^ what ports do we want to listen on?
 
     // // swarm
-    // let (mut client, mut _event_receiver, event_loop) = network::swarm().await?;
-
-    // let mut stdin: io::Lines<io::BufReader<io::Stdin>> = io::BufReader::new(io::stdin()).lines();
-
+    // let (mut client, mut _event_receiver, event_loop) = network::swarm().await?
     // start event loop
     //spawn(event_loop.run());
 
@@ -197,11 +189,45 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
         // gossipsub
         let topic = gossipsub::IdentTopic::new("global-chat");
-        println!("Welcome to the global chat room! Here your messages will be propogated to all peers in the network.");
+        //println!("Welcome to the global chat room! Here your messages will be propogated to all peers in the network.");
         swarm.behaviour_mut().gossipsub.subscribe(&topic)?;
         state.current_room = "global-chat".to_string();
         state.rooms.push("global-chat".to_string());
     }
+
+    // color_eyre::install()?;
+    // let mut terminal: ratatui::Terminal<ratatui::prelude::CrosstermBackend<std::io::Stdout>> = ratatui::init();
+    // let mut app_result = App::new();
+
+    // loop {
+    //     terminal.draw(|frame| app_result.draw(frame))?;
+
+    //     if let Event::Key(key) = event::read()? {
+    //         match app_result.input_mode {
+    //             InputMode::Normal => match key.code {
+    //                 KeyCode::Char('e') => {
+    //                     app_result.input_mode = InputMode::Editing;
+    //                 }
+    //                 KeyCode::Char('q') => {
+    //                     return Ok(());
+    //                 }
+    //                 _ => {}
+    //             },
+    //             InputMode::Editing if key.kind == KeyEventKind::Press => match key.code {
+    //                 KeyCode::Enter => app_result.submit_message(),
+    //                 KeyCode::Char(to_insert) => app_result.enter_char(to_insert),
+    //                 KeyCode::Backspace => app_result.delete_char(),
+    //                 KeyCode::Left => app_result.move_cursor_left(),
+    //                 KeyCode::Right => app_result.move_cursor_right(),
+    //                 KeyCode::Esc => app_result.input_mode = InputMode::Normal,
+    //                 _ => {}
+    //             },
+    //             InputMode::Editing => {}
+    //         }
+    //     }
+    // }
+
+    // ratatui::restore();
 
     loop {
         select! {
@@ -453,62 +479,6 @@ async fn create_command(input: &str) -> Command {
         "/help" => Command::Help,
         _ => Command::Unknown,
     }
-
-    // match parts[0] {
-    //     "/create" => {
-    //         if parts.len() < 2 {
-    //             println!("Usage: /create <room_name>");
-    //         } else {
-    //             let room_name = parts[1].to_string();
-    //             client.create_room(room_name).await;
-    //             println!("Room created");
-    //         }
-    //     }
-    //     // "/join" => {
-    //     //     if parts.len() < 2 {
-    //     //         println!("Usage: /join <room_name>");
-    //     //     } else {
-    //     //         let room_name = parts[1].to_string();
-    //     //         client.join_room(room_name);
-    //     //         println!("Joined room");
-    //     //     }
-    //     // }
-    //     // "/request" => {
-    //     //     if parts.len() < 3 {
-    //     //         println!("Usage: /request <file_name> <peer_id>");
-    //     //     } else {
-    //     //         let file_name = parts[1].to_string();
-    //     //         let peer_id = parts[2].to_string();
-    //     //         client.request_file(peer_id, file_name);
-    //     //         println!("File request sent");
-    //     //     }
-    //     // }
-    //     // "/dm" => {
-    //     //     if parts.len() < 3 {
-    //     //         println!("Usage: /dm <peer_id> <message>");
-    //     //     } else {
-    //     //         let peer_id = parts[1].to_string();
-    //     //         let message = parts[2..].join(" ");
-    //     //         client.direct_message(, );
-    //     //         println!("Direct message sent");
-    //     //     }
-    //     // }
-    //     "/rooms" => {
-    //         println!("Current rooms:");
-    //         client.get_rooms().await;
-    //     }
-    //     // "/nickname" =>{
-    //     //     let local_peer_id_record = kad::RecordKey::new(&self_peer_id.to_string());
-    //     //     let record = kad::Record {
-    //     //         key: local_peer_id_record,
-    //     //         value: args[1].as_bytes().to_vec(),
-    //     //         publisher: None,
-    //     //         expires: None,
-    //     //     };
-    //     //     kademlia
-    //     //         .put_record(record, kad::Quorum::One)
-    //     //         .expect("Failed to store record locally.");
-    //     // }
 }
 
 pub fn handle_command(swarm: &mut Swarm<ChatBehaviour>, command: Command) {
@@ -745,5 +715,168 @@ pub fn handle_command(swarm: &mut Swarm<ChatBehaviour>, command: Command) {
         Command::Unknown => {
             println!("Unknown command! Type /help for a list of available commands.");
         }
+    }
+}
+
+/// App holds the state of the application
+struct App {
+    /// Current value of the input box
+    input: String,
+    /// Position of cursor in the editor area.
+    character_index: usize,
+    /// Current input mode
+    input_mode: InputMode,
+    /// History of recorded messages
+    messages: Vec<String>,
+}
+
+enum InputMode {
+    Normal,
+    Editing,
+}
+
+impl App {
+    const fn new() -> Self {
+        Self {
+            input: String::new(),
+            input_mode: InputMode::Normal,
+            messages: Vec::new(),
+            character_index: 0,
+        }
+    }
+
+    fn move_cursor_left(&mut self) {
+        let cursor_moved_left = self.character_index.saturating_sub(1);
+        self.character_index = self.clamp_cursor(cursor_moved_left);
+    }
+
+    fn move_cursor_right(&mut self) {
+        let cursor_moved_right = self.character_index.saturating_add(1);
+        self.character_index = self.clamp_cursor(cursor_moved_right);
+    }
+
+    fn enter_char(&mut self, new_char: char) {
+        let index = self.byte_index();
+        self.input.insert(index, new_char);
+        self.move_cursor_right();
+    }
+
+    /// Returns the byte index based on the character position.
+    ///
+    /// Since each character in a string can be contain multiple bytes, it's necessary to calculate
+    /// the byte index based on the index of the character.
+    fn byte_index(&self) -> usize {
+        self.input
+            .char_indices()
+            .map(|(i, _)| i)
+            .nth(self.character_index)
+            .unwrap_or(self.input.len())
+    }
+
+    fn delete_char(&mut self) {
+        let is_not_cursor_leftmost = self.character_index != 0;
+        if is_not_cursor_leftmost {
+            // Method "remove" is not used on the saved text for deleting the selected char.
+            // Reason: Using remove on String works on bytes instead of the chars.
+            // Using remove would require special care because of char boundaries.
+
+            let current_index = self.character_index;
+            let from_left_to_current_index = current_index - 1;
+
+            // Getting all characters before the selected character.
+            let before_char_to_delete = self.input.chars().take(from_left_to_current_index);
+            // Getting all characters after selected character.
+            let after_char_to_delete = self.input.chars().skip(current_index);
+
+            // Put all characters together except the selected one.
+            // By leaving the selected one out, it is forgotten and therefore deleted.
+            self.input = before_char_to_delete.chain(after_char_to_delete).collect();
+            self.move_cursor_left();
+        }
+    }
+
+    fn clamp_cursor(&self, new_cursor_pos: usize) -> usize {
+        new_cursor_pos.clamp(0, self.input.chars().count())
+    }
+
+    fn reset_cursor(&mut self) {
+        self.character_index = 0;
+    }
+
+    fn submit_message(&mut self) {
+        self.messages.push(self.input.clone());
+        self.input.clear();
+        self.reset_cursor();
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        let vertical = Layout::vertical([
+            Constraint::Min(1),
+            Constraint::Length(3),
+            Constraint::Length(1),
+        ]);
+        let [messages_area, input_area, help_area] = vertical.areas(frame.area());
+    
+        let messages: Vec<ListItem> = self
+            .messages
+            .iter()
+            .enumerate()
+            .map(|(i, m)| {
+                let content = Line::from(Span::raw(format!("{i}: {m}")));
+                ListItem::new(content)
+            })
+            .collect();
+        let messages = List::new(messages).block(Block::bordered().title("Messages"));
+        frame.render_widget(messages, messages_area);
+    
+        let input = Paragraph::new(self.input.as_str())
+            .style(match self.input_mode {
+                InputMode::Normal => Style::default(),
+                InputMode::Editing => Style::default().fg(Color::Yellow),
+            })
+            .block(Block::bordered().title("Input"));
+        frame.render_widget(input, input_area);
+    
+        match self.input_mode {
+            // Hide the cursor. `Frame` does this by default, so we don't need to do anything here
+            InputMode::Normal => {}
+    
+            // Make the cursor visible and ask ratatui to put it at the specified coordinates after
+            // rendering
+            #[allow(clippy::cast_possible_truncation)]
+            InputMode::Editing => frame.set_cursor_position(Position::new(
+                // Draw the cursor at the current position in the input field.
+                // This position can be controlled via the left and right arrow key
+                input_area.x + self.character_index as u16 + 1,
+                // Move one line down, from the border to the input line
+                input_area.y + 1,
+            )),
+        }
+    
+        let (msg, style) = match self.input_mode {
+            InputMode::Normal => (
+                vec![
+                    "Press ".into(),
+                    "q".bold(),
+                    " to exit, ".into(),
+                    "e".bold(),
+                    " to start editing.".bold(),
+                ],
+                Style::default().add_modifier(Modifier::RAPID_BLINK),
+            ),
+            InputMode::Editing => (
+                vec![
+                    "Press ".into(),
+                    "Esc".bold(),
+                    " to stop editing, ".into(),
+                    "Enter".bold(),
+                    " to record the message".into(),
+                ],
+                Style::default(),
+            ),
+        };
+        let text = Text::from(Line::from(msg)).patch_style(style);
+        let help_message = Paragraph::new(text);
+        frame.render_widget(help_message, help_area);
     }
 }
